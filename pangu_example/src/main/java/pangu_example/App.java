7eeb79cee7fab670649c391a3c4ea334018deacf
Vinta <vinta.chen@gmail.com>
2014-11-27 03:05:32 +0800

    add example project

---
+++ pangu_example/src/main/java/pangu_example/App.java
@@ -0,0 +1,12 @@
+package pangu_example;
+
+import ws.vinta.pangu.Pangu;
+
+public class App {
+    public static void main(String[] args) {
+        Pangu pangu = new Pangu();
+
+        String text = pangu.spacingText("東邪、西毒、南帝、北丐和Ichiro Suzuki五人，為爭一部The Old Man and the Sea，約定在板橋Mega City的3樓比武較量。");
+        System.out.println(text);
+    }
+}
